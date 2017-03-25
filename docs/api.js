cad4401b4668381baeaef0891133fc50d708e16c
Hugo <h@hugo.sx>
2017-03-25 22:51:10 +1100

    Add generated documentation

---
+++ docs/api.js
@@ -0,0 +1,15 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "AccountModel",
+        "OperatingSystem",
+        "Plan",
+        "Region",
+        "Server",
+        "Vultr"
+    ],
+    "modules": [],
+    "allModules": [],
+    "elements": []
+} };
+});
